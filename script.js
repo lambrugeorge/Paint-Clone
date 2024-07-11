34d3cf927d25750d2e2309d71f073c09f25a9117
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-11 21:55:19 +0300

    Canvas Setup

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const downloadBtn = document.getElementById('download');
 const { body } = document;
 
 // Global Variables
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
+
 
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
@@ -63,10 +67,10 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
+  canvas.width = window.innerWidth ;
+  canvas.height = window.innerHeight - 50;
+  context.fillStyle = bucketColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
   body.appendChild(canvas);
 
 }
